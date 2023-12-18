65c46283019599fbe896d00db3e17ce2a506cb30
Merel Jacobs <merel.burleigh@gmail.com>
2023-12-18 15:33:08 -0800

    render fetch

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import useFetch from '../hooks/useFetch';
 
-export default function Home() {
+function Home() {
+    const {loading, error, data} = useFetch('http://localhost:1337/api/people');
+
+    if (loading) return (<p>loading...</p>);
+    if (error) return (<p>error...</p>)
+
+
   return (
     <div>
       <p>Homepage</p>
+      <div>
+      {data.data.map(item => {
+        const { id, attributes } = item;
+        const { Name, Role } = attributes;
+
+        return (
+          <div key={id}>
+            <h2>Name: {Name}</h2>
+            <p>Role: {Role}</p>
+          </div>
+        );
+      })}
+    </div>
     </div>
   )
 }
+
+export default Home;
