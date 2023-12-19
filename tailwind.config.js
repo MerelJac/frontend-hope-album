36682ece783e967654f81becf38200401786af8e
Merel Jacobs <merel.burleigh@gmail.com>
2023-12-18 16:14:37 -0800

    init tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
